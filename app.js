79f30d5bbde65f812a8909d1dfe5141097b4e4b7
XAJX <88792057+XAJX179@users.noreply.github.com>
2024-07-07 10:48:23 +0530

    Feat: Computer's Choice

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log('Hello')
+// Computer's Random Choice From 3 Choices. 
+function getComputerChoice(){
+    let computerChoice = Math.floor(Math.random()*3+1)
+    computerChoice = computerChoice.toString();
+    
+    (computerChoice == '1') ? computerChoice = 'rock':(computerChoice == '2') ? computerChoice = 'paper':(computerChoice == '3') ? computerChoice = 'scissors':
+    '';
+    return computerChoice
+}
+console.log(getComputerChoice());
